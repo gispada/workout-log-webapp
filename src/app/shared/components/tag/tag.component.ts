d8098d8b571d86a4ae1b546b09e1b4c47082d234
Gianluca Spada <gianluca.spada@stackhouse.it>
2022-03-14 09:41:49 +0100

    refactor: tag component

--- src/app/shared/components/tag/tag.component.ts
+++ src/app/shared/components/tag/tag.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core'
+import { Component, Input } from '@angular/core'
 import { hexToRgba } from '@shared/utils'
 
 @Component({
@@ -6,17 +6,25 @@ import { hexToRgba } from '@shared/utils'
   templateUrl: './tag.component.html',
   styleUrls: ['./tag.component.scss']
 })
-export class TagComponent implements OnInit {
+export class TagComponent {
   @Input() color?: string
+  @Input() inverted = false
 
-  style = {}
+  get className() {
+    if (this.color) return ''
+    return `default-color${this.inverted ? '-inverted' : ''}`
+  }
 
-  ngOnInit() {
-    if (this.color) {
-      this.style = {
-        color: this.color,
-        'background-color': hexToRgba(this.color, 0.1)
-      }
-    }
+  get style() {
+    if (!this.color) return null
+    return this.inverted
+      ? {
+          color: '#fafafa',
+          'background-color': hexToRgba(this.color, 0.8)
+        }
+      : {
+          color: this.color,
+          'background-color': hexToRgba(this.color, 0.1)
+        }
   }
 }
